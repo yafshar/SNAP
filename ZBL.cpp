7f71897beea1eaad1519e37ad9654b0a76b438da
Yaser Afshar <ya.afshar@gmail.com>
2019-11-22 13:06:45 -0600

    minor

--- ZBL.cpp
+++ ZBL.cpp
@@ -134,17 +134,22 @@ void ZBL::set_coeff(int const iSpecies,
                     double const qelectron)
 {
     double const ainv = (std::pow(z_iSpecies, ZBLConstants::pzbl) + std::pow(z_jSpecies, ZBLConstants::pzbl)) / (ZBLConstants::a0 * angstrom);
+
     d1a(iSpecies, jSpecies) = ZBLConstants::d1 * ainv;
     d2a(iSpecies, jSpecies) = ZBLConstants::d2 * ainv;
     d3a(iSpecies, jSpecies) = ZBLConstants::d3 * ainv;
     d4a(iSpecies, jSpecies) = ZBLConstants::d4 * ainv;
+
     zze(iSpecies, jSpecies) = z_iSpecies * z_jSpecies * qqr2e * qelectron * qelectron;
 
-    d1a(jSpecies, iSpecies) = d1a(iSpecies, jSpecies);
-    d2a(jSpecies, iSpecies) = d2a(iSpecies, jSpecies);
-    d3a(jSpecies, iSpecies) = d3a(iSpecies, jSpecies);
-    d4a(jSpecies, iSpecies) = d4a(iSpecies, jSpecies);
-    zze(jSpecies, iSpecies) = zze(iSpecies, jSpecies);
+    if (iSpecies != jSpecies)
+    {
+        d1a(jSpecies, iSpecies) = d1a(iSpecies, jSpecies);
+        d2a(jSpecies, iSpecies) = d2a(iSpecies, jSpecies);
+        d3a(jSpecies, iSpecies) = d3a(iSpecies, jSpecies);
+        d4a(jSpecies, iSpecies) = d4a(iSpecies, jSpecies);
+        zze(jSpecies, iSpecies) = zze(iSpecies, jSpecies);
+    }
 
     double const tc = cut_global - cut_inner;
 
@@ -162,11 +167,14 @@ void ZBL::set_coeff(int const iSpecies,
     sw4(iSpecies, jSpecies) = swb / 4.0;
     sw5(iSpecies, jSpecies) = swc;
 
-    sw1(jSpecies, iSpecies) = sw1(iSpecies, jSpecies);
-    sw2(jSpecies, iSpecies) = sw2(iSpecies, jSpecies);
-    sw3(jSpecies, iSpecies) = sw3(iSpecies, jSpecies);
-    sw4(jSpecies, iSpecies) = sw4(iSpecies, jSpecies);
-    sw5(jSpecies, iSpecies) = sw5(iSpecies, jSpecies);
+    if (iSpecies != jSpecies)
+    {
+        sw1(jSpecies, iSpecies) = sw1(iSpecies, jSpecies);
+        sw2(jSpecies, iSpecies) = sw2(iSpecies, jSpecies);
+        sw3(jSpecies, iSpecies) = sw3(iSpecies, jSpecies);
+        sw4(jSpecies, iSpecies) = sw4(iSpecies, jSpecies);
+        sw5(jSpecies, iSpecies) = sw5(iSpecies, jSpecies);
+    }
 }
 
 double ZBL::e_zbl(double const rij, int const iSpecies, int const jSpecies)
@@ -177,10 +185,11 @@ double ZBL::e_zbl(double const rij, int const iSpecies, int const jSpecies)
     double const d4aij = d4a(iSpecies, jSpecies);
     double const zzeij = zze(iSpecies, jSpecies);
 
-    double sum = ZBLConstants::c1 * std::exp(-d1aij * rij);
-    sum += ZBLConstants::c2 * std::exp(-d2aij * rij);
-    sum += ZBLConstants::c3 * std::exp(-d3aij * rij);
-    sum += ZBLConstants::c4 * std::exp(-d4aij * rij);
+    double const sum1 = ZBLConstants::c1 * std::exp(-d1aij * rij);
+    double const sum2 = ZBLConstants::c2 * std::exp(-d2aij * rij);
+    double const sum3 = ZBLConstants::c3 * std::exp(-d3aij * rij);
+    double const sum4 = ZBLConstants::c4 * std::exp(-d4aij * rij);
+    double const sum = sum1 + sum2 + sum3 + sum4;
 
     double const rinv = 1.0 / rij;
 
@@ -194,6 +203,7 @@ double ZBL::dzbldr(double const rij, int const iSpecies, int const jSpecies)
     double const d2aij = d2a(iSpecies, jSpecies);
     double const d3aij = d3a(iSpecies, jSpecies);
     double const d4aij = d4a(iSpecies, jSpecies);
+
     double const zzeij = zze(iSpecies, jSpecies);
 
     double const e1 = std::exp(-d1aij * rij);
@@ -201,19 +211,21 @@ double ZBL::dzbldr(double const rij, int const iSpecies, int const jSpecies)
     double const e3 = std::exp(-d3aij * rij);
     double const e4 = std::exp(-d4aij * rij);
 
-    double sum = ZBLConstants::c1 * e1;
-    sum += ZBLConstants::c2 * e2;
-    sum += ZBLConstants::c3 * e3;
-    sum += ZBLConstants::c4 * e4;
+    double const sum1 = ZBLConstants::c1 * e1;
+    double const sum2 = ZBLConstants::c2 * e2;
+    double const sum3 = ZBLConstants::c3 * e3;
+    double const sum4 = ZBLConstants::c4 * e4;
+    double const sum = sum1 + sum2 + sum3 + sum4;
 
-    double sum_p = -ZBLConstants::c1 * d1aij * e1;
-    sum_p -= ZBLConstants::c2 * d2aij * e2;
-    sum_p -= ZBLConstants::c3 * d3aij * e3;
-    sum_p -= ZBLConstants::c4 * d4aij * e4;
+    double const sum_p1 = ZBLConstants::c1 * d1aij * e1;
+    double const sum_p2 = ZBLConstants::c2 * d2aij * e2;
+    double const sum_p3 = ZBLConstants::c3 * d3aij * e3;
+    double const sum_p4 = ZBLConstants::c4 * d4aij * e4;
+    double const sum_p = sum_p1 + sum_p2 + sum_p3 + sum_p4;
 
     double const rinv = 1.0 / rij;
 
-    double const result = zzeij * (sum_p - sum * rinv) * rinv;
+    double const result = zzeij * (-sum_p - sum * rinv) * rinv;
 
     return result;
 }
@@ -231,20 +243,23 @@ double ZBL::d2zbldr2(double const rij, int const iSpecies, int const jSpecies)
     double const e3 = std::exp(-d3aij * rij);
     double const e4 = std::exp(-d4aij * rij);
 
-    double sum = ZBLConstants::c1 * e1;
-    sum += ZBLConstants::c2 * e2;
-    sum += ZBLConstants::c3 * e3;
-    sum += ZBLConstants::c4 * e4;
+    double const sum1 = ZBLConstants::c1 * e1;
+    double const sum2 = ZBLConstants::c2 * e2;
+    double const sum3 = ZBLConstants::c3 * e3;
+    double const sum4 = ZBLConstants::c4 * e4;
+    double const sum = sum1 + sum2 + sum3 + sum4;
 
-    double sum_p = ZBLConstants::c1 * e1 * d1aij;
-    sum_p += ZBLConstants::c2 * e2 * d2aij;
-    sum_p += ZBLConstants::c3 * e3 * d3aij;
-    sum_p += ZBLConstants::c4 * e4 * d4aij;
+    double const sum_p1 = ZBLConstants::c1 * e1 * d1aij;
+    double const sum_p2 = ZBLConstants::c2 * e2 * d2aij;
+    double const sum_p3 = ZBLConstants::c3 * e3 * d3aij;
+    double const sum_p4 = ZBLConstants::c4 * e4 * d4aij;
+    double const sum_p = sum_p1 + sum_p2 + sum_p3 + sum_p4;
 
-    double sum_pp = ZBLConstants::c1 * e1 * d1aij * d1aij;
-    sum_pp += ZBLConstants::c2 * e2 * d2aij * d2aij;
-    sum_pp += ZBLConstants::c3 * e3 * d3aij * d3aij;
-    sum_pp += ZBLConstants::c4 * e4 * d4aij * d4aij;
+    double const sum_pp1 = ZBLConstants::c1 * e1 * d1aij * d1aij;
+    double const sum_pp2 = ZBLConstants::c2 * e2 * d2aij * d2aij;
+    double const sum_pp3 = ZBLConstants::c3 * e3 * d3aij * d3aij;
+    double const sum_pp4 = ZBLConstants::c4 * e4 * d4aij * d4aij;
+    double const sum_pp = sum_pp1 + sum_pp2 + sum_pp3 + sum_pp4;
 
     double const rinv = 1.0 / rij;
 
